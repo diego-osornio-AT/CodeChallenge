560ebed110c6ee9c521f6c76c52650b351289c5a
Diego Osornio <diego.osornio@agilethought.com>
2022-09-13 13:14:38 -0500

    General fixes and solution improvements

--- Application/Program.cs
+++ Application/Program.cs
@@ -11,26 +11,32 @@ namespace ConsoleApplication
         {
             try
             {
-                var sortService = new SortService(new LengthValidator());
+                /*
+                 * Sort Service and Validator are not injected using a DI Container since this is a simple
+                 * console application, if we wanted to re-use the same functionality in different places everything
+                 * is plug and play, we can easily switch console application to web api and register our services
+                 * in a DI conter since our core does not depend on the Console Application
+               */
+                var sortService = new SortService(new Validator());
                 var words = new[] { "Sonia", "Maria", "Wood", "Dempster" };
-                var sortOrder = new[] { 4, 1, 2, 3 };
+                var sortOrder = new[] { 4, 1, 3, 2 };
 
+                Console.WriteLine("Sort Order has to start on 1");
                 Console.WriteLine($"Initial words order: {string.Join(',', words)}");
-                Console.WriteLine($"Initial  sort order: {string.Join(',', sortOrder)}");
+                Console.WriteLine($"Initial sort order: {string.Join(',', sortOrder)}");
 
                 var sortedWords = sortService.SortByPosition(words, sortOrder);
                 Console.WriteLine($"Sorted array: {string.Join(',', sortedWords)}");
             }
-            //Is this acceptable?
-            catch (DuplicatedPositionException e)
-            { Console.WriteLine(e); }
-            catch (OutOfRangeException e)
-            { Console.WriteLine(e); }
-            catch (WordsAndPositionsMiscountException e)
-            { Console.WriteLine(e); }
             catch (NullReferenceException e)
             { Console.WriteLine(e); }
             catch (ArgumentNullException e)
+            { Console.WriteLine(e); }
+            catch (DuplicatedPositionException e)
+            { Console.WriteLine(e); }
+            catch (NonSequentialCollectionException e)
+            { Console.WriteLine(e); }
+            catch (WordsAndPositionsMiscountException e)
             { Console.WriteLine(e); }
         }
     }
---
+++ Domain/Exceptions/NonSequentialCollectionException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Domain.Exceptions
+{
+    public class NonSequentialCollectionException : Exception
+    {
+        /// <summary>
+        /// Thrown when collection is non sequential and not starting at 1 with max value of collection.Length
+        /// </summary>
+        public NonSequentialCollectionException() : base("Collection is not sequential starting on 1")
+        {
+        }
+    }
+}
